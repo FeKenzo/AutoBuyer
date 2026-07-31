bdfa247f8b3e04b637c6c3ee2fc81e783a3ee492
Felipe Kenzo <felipeozako@gmail.com>
2026-07-31 19:16:12 -0300

    feat: create initial domain entities

---
+++ src/AutoBuyer.Domain/Entities/ProductTarget.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoBuyer.Domain.Entities
+{
+    internal class ProductTarget
+    {
+    }
+}
